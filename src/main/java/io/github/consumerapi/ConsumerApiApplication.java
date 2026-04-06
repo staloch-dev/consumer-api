6a9eaa526e756da7842efbe18e71571e25ad3840
Jaison Staloch Junior | Desktop-Home <staloch.dev@gmail.com>
2026-04-06 20:05:06 -0300

    chore: configure application properties

--- src/main/java/io/github/consumerapi/ConsumerApiApplication.java
+++ src/main/java/io/github/consumerapi/ConsumerApiApplication.java
@@ -2,12 +2,15 @@ package io.github.consumerapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @SpringBootApplication
 public class ConsumerApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumerApiApplication.class, args);
+		log.info("Application started successfully!");
 	}
 
 }
