5c663c917a5c578d96deb7ba501ed37023a0c17d
Jaison Staloch Junior | Desktop-DevCourse <staloch.dev@gmail.com>
2026-04-07 20:35:11 -0300

    chore: configure swagger infrastructure

--- src/main/java/io/github/consumerapi/ConsumerApiApplication.java
+++ src/main/java/io/github/consumerapi/ConsumerApiApplication.java
@@ -2,6 +2,7 @@ package io.github.consumerapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -9,8 +10,11 @@ import lombok.extern.slf4j.Slf4j;
 public class ConsumerApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConsumerApiApplication.class, args);
+		var context =SpringApplication.run(ConsumerApiApplication.class, args);
+		String port = context.getEnvironment().getProperty("server.port", "8080");
 		log.info("Application started successfully!");
+		log.info("Swagger UI: http://localhost:{}/swagger-ui.html", port);
+
 	}
 
 }
