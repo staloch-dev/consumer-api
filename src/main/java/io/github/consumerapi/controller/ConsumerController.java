5d74c3b642b701a735b980ee8274884da74e789a
Jaison Staloch Junior | Desktop-DevCourse <staloch.dev@gmail.com>
2026-04-13 19:32:58 -0300

    feat: enable CORS for all origins in ConsumerController

--- src/main/java/io/github/consumerapi/controller/ConsumerController.java
+++ src/main/java/io/github/consumerapi/controller/ConsumerController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import jakarta.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import io.github.consumerapi.dto.ConsumerRequestDTO;
 import io.github.consumerapi.dto.ConsumerResponseDTO;
 import io.github.consumerapi.service.ConsumerService;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/consumers")
 @RequiredArgsConstructor
